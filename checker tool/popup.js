71047e3cc97d370ecf5666bf723b7027fe61bf10
aman-pratap-s <61133447+aman-pratap-s@users.noreply.github.com>
2024-10-01 21:01:52 +0530

    Init: JS Tool

---
+++ checker tool/popup.js
@@ -0,0 +1,38 @@
+function checkAccessibility() {
+  chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
+    const tab = tabs[0];
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id}, 
+      files: ["axe.min.js"]
+    }).then(() => {
+      chrome.scripting.executeScript({ 
+        target: { tabId: tab.id},
+        function: function () {
+          axe.run(document, {}, (err, results) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            results.violations.forEach (violation => {
+              violation.nodes.forEach(node => {
+                const reason = node.failureSummary; 
+                const selector = node.target.join(",");
+                const elements = document.querySelectorAll(selector);
+
+                elements.forEach(element => {
+                  element.title = reason;
+                  element.style.border = "2px solid red";
+                });
+              });
+            });
+          });
+        }
+      });
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const checkButton = document.getElementById('checkButton');
+  checkButton.addEventListener('click', checkAccessibility);
+});
